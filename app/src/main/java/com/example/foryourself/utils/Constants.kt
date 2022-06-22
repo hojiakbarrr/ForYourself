4195427217d0d3e21c19bc7bdd797742639babbe
hojiakbarrr <hojiakbar_1997@mail.ru>
2022-06-22 19:42:19 +0600

    ...

--- app/src/main/java/com/example/foryourself/utils/Constants.kt
+++ app/src/main/java/com/example/foryourself/utils/Constants.kt
@@ -2,10 +2,11 @@ package com.example.foryourself.utils
 
 
 object Constants {
-    const val APPLICATION_ID: String = "9TvPKN6kO8NZVihoWKkE3lc9YjWrgxyNDjadED2O"
-    const val CLIENT_KEY: String = "CsCk0il7BkqjDi5qKbCxeErpD2yrqwVNIp923YKg"
-    const val REST_API_KEY: String = "HvsbgFcXJoZSqrFJXHYYPqxtb3ZeCJ02glXYKSW1"
-    const val BASE_URL: String = "https://parseapi.back4app.com/"
+    const val APPLICATION_ID: String = ""
+    const val CLIENT_KEY: String = ""
+    const val REST_API_KEY: String = ""
+    const val BASE_URL: String = ""
     const val CONTENT_TYPE: String = "application/json"
+
 
 }
